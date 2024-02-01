7ec862048b63facb0bf6bb7e6d0626265cada3b2
akiselev1983 <akiselev.brd@gmail.com>
2024-02-01 14:25:18 +0200

    main

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const Character = ({value}) => {
-    const {id,name,status,species,gender,image} = value;
+    const {id,name,status,species,gender,image,created} = value;
 
     return (
         <div>
-            <div>{id}</div>
+            <div>id {id}</div>
             <div>{name}</div>
             <div>{status}</div>
             <div>{species}</div>
             <div>{gender}</div>
+            <div>{created}</div>
             <img src={image} alt={name}/>
+            <hr/>
 
         </div>
     );
