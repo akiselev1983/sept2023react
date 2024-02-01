0a6c2fb98c7eb4f61045ae2ca4a0c2d7f8f8b6a9
akiselev1983 <akiselev.brd@gmail.com>
2024-02-01 14:11:47 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import Simpsons from "./components/Simpsons/Simpsons";
-import Characters from "./components/Characters/Characters";
 
 const App = () => {
     return (
         <div>
-            {/*<Simpsons/>*/}
-            <Characters/>
+            App
         </div>
     );
 };
 
 export default App;
 
-// 1. Описати всю сім'ю сімпсонів (5 персонажів)
-//
-// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
-// https://rickandmortyapi.com/
-//     https://rickandmortyapi.com/api/character
-//         Створити 6 персонажів
