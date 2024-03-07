dc57e76b64ae9ebba3c7a2f14372b3733361f2b5
akiselev1983 <akiselev.brd@gmail.com>
2024-03-07 12:20:46 +0200

    main

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import {RouterProvider} from "react-router-dom";
-import {router} from "./router";
-import {Provider} from "react-redux";
-import {store} from "./store/store";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <RouterProvider router={router}/>
-    </Provider>
+   <App/>
 );
 
